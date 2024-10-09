c68a7f816df97c7e3867ee88f1d0eb773bbf1145
Neon <neonfamxxi@gmail.com>
2024-10-08 20:18:21 -0700

    finished todo!

--- todoCli/src/main.rs
+++ todoCli/src/main.rs
@@ -15,10 +15,10 @@ const PAGE_LENGTH: usize = 8;
 
 fn find_file() -> String {
     let file_path;
-    if !fs::metadata(FILE_NAME).is_ok() {
+    let current_dir = env::current_dir().unwrap();
+    let new_dir = &current_dir.parent().unwrap().join(FILE_NAME);
+    if !fs::metadata(FILE_NAME).is_ok() && fs::metadata(new_dir).is_ok() {
         //if not found check in parent directory
-        let current_dir = env::current_dir().unwrap();
-        let new_dir = current_dir.parent().unwrap().join(FILE_NAME);
         file_path = new_dir.to_str().unwrap().to_owned();
     } else {
         file_path = FILE_NAME.to_string();
@@ -27,12 +27,25 @@ fn find_file() -> String {
 }
 
 fn get_tasks() -> Vec<Task> {
-    let file_path = find_file();
-
-    return match fs::read_to_string(file_path) {
+    return match fs::read_to_string(find_file()) {
         Ok(content) => serde_json::from_str(&content).unwrap(),
         Err(_) => Vec::new(),
-    }; //.to_owned();
+    };
+}
+
+fn save_tasks(tasks: Vec<Task>) {
+    fs::write(find_file(), serde_json::to_string_pretty(&tasks).unwrap()).unwrap();
+}
+
+fn format_task(index: usize, task: &Task) -> String {
+    let mut tags_text: String = task.tags.get(0).unwrap_or(&"".to_string()).to_string();
+
+    for j in 1..task.tags.len() {
+        tags_text.push_str(", ");
+        tags_text.push_str(task.tags.get(j).unwrap());
+    }
+
+    return format!(" ({}) [{}] {}", index, tags_text, task.text);
 }
 
 fn help_usage(cmd: &str) -> &str {
@@ -69,14 +82,14 @@ fn do_help(args: &Vec<String>) {
         println!(
             "\nNAME:\n\t\t{}-{} - {}\n",
             CMD_NAME,
-            args[3],
-            help_desc(args[3].as_str())
+            args[2],
+            help_desc(args[2].as_str())
         );
         println!(
             "SYNOPSIS:\n\t\t{} {} {}\n",
             CMD_NAME,
-            args[3],
-            help_usage(args[3].as_str())
+            args[2],
+            help_usage(args[2].as_str())
         );
         println!("Extended help for commands coming soon."); // TODO: help_options(), help_full()
     } else {
@@ -87,7 +100,7 @@ fn do_help(args: &Vec<String>) {
         println!("\tadd, a          {}", help_desc("add"));
         println!("\tremove, r       {}", help_desc("remove"));
         println!("\tlist, l         {}", help_desc("list"));
-        println!("\tsearch          {}", help_desc("search"));
+        println!("\tsearch, s       {}", help_desc("search"));
         println!("\tcomplete, c     {}", help_desc("complete"));
         println!("\tdelete          {}", help_desc("delete")); //should there be a recovery thing??
         println!("\tclean           {}", help_desc("clean"));
@@ -104,6 +117,14 @@ fn new_task(args: Vec<String>) {
     }
 
     let mut tasks = get_tasks();
+
+    tasks.push(Task {
+        text: args.get(2).unwrap().to_string(),
+        completed: false,
+        tags: Vec::new(),
+    });
+
+    save_tasks(tasks);
 }
 
 fn add_task(args: Vec<String>) {
@@ -126,10 +147,7 @@ fn add_task(args: Vec<String>) {
         }
     }
 
-    // Save the updated tasks back to the file
-    let file_path = find_file();
-    let serialized_tasks = serde_json::to_string(&tasks).unwrap();
-    fs::write(file_path, serialized_tasks).unwrap();
+    save_tasks(tasks);
 }
 
 fn remove_task(args: Vec<String>) {
@@ -151,22 +169,26 @@ fn remove_task(args: Vec<String>) {
             Some(val) => tag = val,
             None => continue,
         }
-        match args.find(tag) {
-            Some(index) => tags.swap_remove(index),
-            None => (),
+
+        for i in 0..tags.len() {
+            match tags.get(i) {
+                Some(val) => {
+                    if tag == val {
+                        tags.swap_remove(i);
+                    }
+                }
+                None => (),
+            }
         }
     }
 
-    // Save the updated tasks back to the file
-    let file_path = find_file();
-    let serialized_tasks = serde_json::to_string(&tasks).unwrap();
-    fs::write(file_path, serialized_tasks).unwrap();
+    save_tasks(tasks);
 }
 
 fn list_task(args: Vec<String>) {
     let tasks = get_tasks();
     let mut page: usize = 0;
-    if args.len() >= 3 {
+    if args.len() > 2 {
         page = args[2].parse().unwrap();
     }
 
@@ -175,26 +197,79 @@ fn list_task(args: Vec<String>) {
     let start: usize = cmp::min(maxxed * PAGE_LENGTH, 0);
 
     for i in start..start + 20 {
-        let t = tasks.get(i).unwrap(); // TODO: figure out how to get the tasks from this
-        let mut tags_text: String = t.tags.get(0).unwrap_or(&"".to_string()).to_string();
-
-        for j in 1..t.tags.len() {
-            tags_text.push_str(", ");
-            tags_text.push_str(t.tags.get(j).unwrap());
-        }
-
-        println!(" ({}) [{}] {}", i, tags_text, t.text);
+        let t = match tasks.get(i) {
+            Some(v) => v,
+            None => continue,
+        };
+        println!("{}", format_task(i, &t));
     }
+    println!(
+        "\nPage {} of {}. Use '{} list [PAGE NUMBER]' to for more results.",
+        maxxed,
+        len / PAGE_LENGTH,
+        CMD_NAME
+    );
 }
 
 fn search_task(args: Vec<String>) {
     if args.len() < 3 {
         return println!(
-            "No page number provided.\nTry '{} help search' for more details.",
+            "No tags to search by provided.\nTry '{} help search' for more details.",
             CMD_NAME
         );
     }
+
+    let tasks = get_tasks();
+    let tags = &args[2..];
+    for (i, v) in tasks.iter().enumerate() {
+        let mut success = true;
+        for tag in tags {
+            if !v.tags.contains(tag) {
+                success = false;
+                break;
+            }
+        }
+        if !success {
+            continue;
+        }
+        println!("{}", format_task(i, &v));
+    }
+}
+
+fn complete_task(args: Vec<String>) {
+    if args.len() < 3 {
+        println!(
+            "No task number provided.\nTry '{} help complete' for more details.",
+            CMD_NAME
+        );
+        return;
+    }
+
+    let task_number: usize = args[2].parse().unwrap();
     let mut tasks = get_tasks();
+    tasks.get_mut(task_number).unwrap().completed = true;
+
+    save_tasks(tasks);
+}
+
+fn delete_task(args: Vec<String>) {
+    if args.len() < 3 {
+        println!(
+            "No task bumber provided.\nTry '{} help delete' for more details.",
+            CMD_NAME
+        );
+        return;
+    }
+
+    let task_number: usize = args[2].parse().unwrap();
+    let mut tasks = get_tasks();
+
+    tasks.remove(task_number);
+    save_tasks(tasks);
+}
+
+fn clean_task() {
+    save_tasks(Vec::new());
 }
 
 fn main() {
@@ -202,8 +277,9 @@ fn main() {
 
     if args.len() < 2 {
         println!("Welcome to Max's custom todo cli!");
-        println!("Version - 0.1\n\n");
+        println!("Version - 0.1\n");
         do_help(&args);
+        return;
     }
 
     //figure out whether user wants to
@@ -212,16 +288,16 @@ fn main() {
         "n" => new_task(args),
         "add" => add_task(args),
         "a" => add_task(args),
-        /*  "remove" => new_task(args),
-            "r" => new_task(args),
-            "list" => new_task(args),
-            "l" => new_task(args),
-            "search" => new_task(args),
-            "complete" => new_task(args),
-            "c" => new_task(args),
-            "delete" => new_task(args),
-            "clean" => new_task(args),
-        */
+        "remove" => remove_task(args),
+        "r" => remove_task(args),
+        "list" => list_task(args),
+        "l" => list_task(args),
+        "search" => search_task(args),
+        "s" => search_task(args),
+        "complete" => complete_task(args),
+        "c" => complete_task(args),
+        "delete" => delete_task(args),
+        "clean" => clean_task(),
         &_ => do_help(&args),
     }
 }
---
+++ todoCli/target/debug/todoCli.d
@@ -0,0 +1,1 @@
+C:\Users\neonf\Documents\Github\rust_practice\todoCli\target\debug\todoCli.exe: C:\Users\neonf\Documents\Github\rust_practice\todoCli\src\main.rs
