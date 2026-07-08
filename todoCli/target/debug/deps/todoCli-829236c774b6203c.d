84958f2b10c5bb16a344e13c1cc03b2a337cba9a
Max Bennett <neonfamxxi@gmail.com>
2026-07-08 10:39:51 -0700

    Delete todoCli/target directory

--- todoCli/target/debug/deps/todoCli-829236c774b6203c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\neonf\Documents\GitHub\rust_practice\todoCli\target\debug\deps\libtodoCli-829236c774b6203c.rmeta: src/main.rs
-
-C:\Users\neonf\Documents\GitHub\rust_practice\todoCli\target\debug\deps\todoCli-829236c774b6203c.d: src/main.rs
-
-src/main.rs:
--- todoCli/target/debug/deps/todoCli-fd0887e85e71c65f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\neonf\Documents\Github\rust_practice\todoCli\target\debug\deps\libtodoCli-fd0887e85e71c65f.rmeta: src/main.rs
-
-C:\Users\neonf\Documents\Github\rust_practice\todoCli\target\debug\deps\todoCli-fd0887e85e71c65f.d: src/main.rs
-
-src/main.rs:
